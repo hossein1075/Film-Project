7fcfe242169748ecdc4c6a57aebeb48f8be2c31f
hossein hosseini <hosseinh1075@gmail.com>
2026-02-03 20:21:35 +0330

    complete header desktop

--- src/Components/Header/NavDesktop.tsx
+++ src/Components/Header/NavDesktop.tsx
@@ -13,7 +13,7 @@ import './befores.css'
 function NavDesktop() {
     const {theme, toggleTheme} = useTheme()
   return (
-    <nav>
+    <nav className='hidden lg:block'>
         <div className="container">
             <div className='flex justify-between py-4'>
             <div className='flex basis-[70%] gap-5'>
@@ -25,71 +25,15 @@ function NavDesktop() {
                      <li>
                         <Link to='#'>صفحه اصلی</Link>
                      </li>
-                     <li className='relative group'>
-                        <Link to='#' className='flexCenter gap-0.5'>
-                        <IoVideocamOutline />
-                        فیلم
-                        <RiArrowDownSLine/>
-                        </Link>
-                        {/* subMenu */}
-                        <div className='absolute top-9 opacity-0 invisible backdrop-blur-md group-hover:opacity-100 group-hover:visible w-52 z-40 bg-subMenu/20 light:bg-subMenu/90 transition-all duration-150 delay-75 shadow-[0px_0px_15px_#101014e0] rounded-10px'>
-                            <ul className='py-2.5 beforeSubFirst'>
-                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
-                                    <Link to='#'>اکشن</Link>
-                                </li>
-                                <li className='w-full py-2 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
-                                    <Link to='#'>کمدی</Link>
-                                </li>
-                                <li className='w-full py-2 px-3.5 transition-all duration-150 delay-75 hover:bg-blue-500/40 text-13px'>
-                                    <Link to='#' className='flex items-center gap-0.5 text-BluesubMenu'>
-                                    مشاهده همه فیلم‌ها 
-                                    <MdOutlineKeyboardArrowLeft />
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                     </li>
-                     <li className='relative group'>
-                        <Link to='#' className='flexCenter gap-0.5'>
-                        سریال
-                        <RiArrowDownSLine/>
-                        </Link>
-                        {/* subMenu */}
-                         <div className='absolute top-9 opacity-0 invisible backdrop-blur-md group-hover:opacity-100 group-hover:visible w-52 z-40 bg-subMenu/20 light:bg-subMenu/90 transition-all duration-150 delay-75 shadow-[0px_0px_15px_#101014e0] rounded-10px'>
-                            <ul className='py-2.5 beforeSubFirst'>
-                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
-                                    <Link to='#'>اکشن</Link>
-                                </li>
-                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
-                                    <Link to='#'>درام</Link>
-                                </li>
-                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
-                                    <Link to='#'>جنایی</Link>
-                                </li>
-                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
-                                    <Link to='#'>رازآلود</Link>
-                                </li>
-                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
-                                    <Link to='#'>اجتماعی</Link>
-                                </li>
-                                <li className='w-full py-2 px-3.5 transition-all duration-150 delay-75 hover:bg-blue-500/40 text-13px'>
-                                    <Link to='#' className='flex items-center gap-0.5 text-BluesubMenu'>
-                                    مشاهده همه سریال‌ها 
-                                    <MdOutlineKeyboardArrowLeft />
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                     </li>
-                     <li className='relative group'>
+                      <li className='relative group'>
                         <Link to='#' className='flexCenter gap-0.5'>
                         <BsJustify />
-                        صفحات اختصاصی کلاکت فیلم
+                        صفحات اختصاصی کلاکت 
                         <RiArrowDownSLine/>
                         </Link>
                         {/* subMenu */}
-                         <div className='absolute top-9 opacity-0 invisible backdrop-blur-md group-hover:opacity-100 group-hover:visible w-200 z-40 bg-subMenu/20 light:bg-subMenu/90 transition-all duration-150 delay-75 shadow-[0px_0px_15px_#101014e0] rounded-10px'>
-                            <ul className='p-5 beforeSubFirst beforeDotLists grid grid-cols-3'>
+                         <div className='absolute top-10 opacity-0 invisible backdrop-blur-md group-hover:opacity-100 group-hover:visible w-135 2xl:w-200 z-40 bg-subMenu/20 light:bg-subMenu/90 transition-all duration-150 delay-75 shadow-[0px_0px_15px_#101014e0] rounded-10px'>
+                            <ul className='p-5 beforeSubFirst beforeDotLists grid grid-cols-2 2xl:grid-cols-3'>
                                 <li className='w-full py-1.5 px-3.5 text-13px'>
                                     
                                     <Link to='#' className='p-1.5 transition-all duration-150 delay-300 hover:text-blue-700 hover:mr-2.5'>250 فیلم برتر imdb</Link>
@@ -141,6 +85,63 @@ function NavDesktop() {
                             </ul>
                         </div>
                      </li>
+                     <li className='relative group'>
+                        <Link to='#' className='flexCenter gap-0.5'>
+                        <IoVideocamOutline />
+                        فیلم
+                        <RiArrowDownSLine/>
+                        </Link>
+                        {/* subMenu */}
+                        <div className='absolute top-10 opacity-0 invisible backdrop-blur-md group-hover:opacity-100 group-hover:visible w-52 z-40 bg-subMenu/20 light:bg-subMenu/90 transition-all duration-150 delay-75 shadow-[0px_0px_15px_#101014e0] rounded-10px'>
+                            <ul className='py-2.5 beforeSubFirst'>
+                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
+                                    <Link to='#'>اکشن</Link>
+                                </li>
+                                <li className='w-full py-2 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
+                                    <Link to='#'>کمدی</Link>
+                                </li>
+                                <li className='w-full py-2 px-3.5 transition-all duration-150 delay-75 hover:bg-blue-500/40 text-13px'>
+                                    <Link to='#' className='flex items-center gap-0.5 text-BluesubMenu'>
+                                    مشاهده همه فیلم‌ها 
+                                    <MdOutlineKeyboardArrowLeft />
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                     </li>
+                     <li className='relative group'>
+                        <Link to='#' className='flexCenter gap-0.5'>
+                        سریال
+                        <RiArrowDownSLine/>
+                        </Link>
+                        {/* subMenu */}
+                         <div className='absolute top-10 opacity-0 invisible backdrop-blur-md group-hover:opacity-100 group-hover:visible w-52 z-40 bg-subMenu/20 light:bg-subMenu/90 transition-all duration-150 delay-75 shadow-[0px_0px_15px_#101014e0] rounded-10px'>
+                            <ul className='py-2.5 beforeSubFirst'>
+                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
+                                    <Link to='#'>اکشن</Link>
+                                </li>
+                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
+                                    <Link to='#'>درام</Link>
+                                </li>
+                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
+                                    <Link to='#'>جنایی</Link>
+                                </li>
+                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
+                                    <Link to='#'>رازآلود</Link>
+                                </li>
+                                <li className='w-full py-1.5 px-3.5 transition-all duration-150 delay-75 hover:bg-hover-subMenu text-13px'>
+                                    <Link to='#'>اجتماعی</Link>
+                                </li>
+                                <li className='w-full py-2 px-3.5 transition-all duration-150 delay-75 hover:bg-blue-500/40 text-13px'>
+                                    <Link to='#' className='flex items-center gap-0.5 text-BluesubMenu'>
+                                    مشاهده همه سریال‌ها 
+                                    <MdOutlineKeyboardArrowLeft />
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                     </li>
+                    
                 </ul>
             </div>
             <div className='flex gap-2.5'>
@@ -154,7 +155,7 @@ function NavDesktop() {
                     <button className='bg-zinc-900 light:bg-white light:*:text-blue-500 py-3.5 px-4 rounded-15px opacity-100 hover:opacity-90 transition-all delay-100 duration-300'>
                         <IoSearchOutline size={22}/>
                     </button>
-                <button className='font-Estedad-SemiBold text-sm bg-blue-700 light:bg-white light:text-blue-500 py-3.5 px-4 rounded-15px opacity-100 hover:opacity-90 transition-all delay-100 duration-300'>ورود / عضویت</button>
+                <button className='font-Estedad-SemiBold text-xs xl:text-sm bg-blue-700 light:bg-white light:text-blue-500 py-3.5 px-4 rounded-15px opacity-100 hover:opacity-90 transition-all delay-100 duration-300'>ورود / عضویت</button>
             </div>
         </div>
         </div>
