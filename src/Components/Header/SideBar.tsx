0702f6f20e8c60b8f936dd10406174a6be711b6b
hossein hosseini <hosseinh1075@gmail.com>
2026-02-07 19:43:24 +0330

    add css part1 sideBar header

--- src/Components/Header/SideBar.tsx
+++ src/Components/Header/SideBar.tsx
@@ -5,45 +5,47 @@ import { Link } from 'react-router-dom'
 import useTheme from '../useTheme/UseTheme'
 
 function SideBar() {
-  const {theme, toggleTheme} = useTheme()
+  const { theme, toggleTheme } = useTheme()
   return (
-    <div className='w-100 fixed top-0 h-full bg-black'>
+    <div className='w-[70%] fixed top-0 h-full bg-zinc-900 z-50 light:bg-slate-50'>
 
       <div className='flexCenter gap-1'>
         <img src="/Icons/Favicon/logo.png" className='size-10' alt="film-site" />
-        <span className='font-Estedad-Bold'>کلاکت فیلم</span>
+        <span className='font-Estedad-Bold text-white my-5'>کلاکت فیلم</span>
       </div>
 
-      <div>
+      <div className='pb-4'>
         <ul>
-          <li>
-            <Link to=''>فیلم</Link>
+          <li className='border-solid border-zinc-800 light:border-blue-100 border-b-2 text-neutral-300 light:text-slate-500 text-sm p-3.25'>
+            <Link to='#'>فیلم</Link>
           </li>
-          <li>
-            <Link to=''>سریال</Link>
+          <li className='border-solid border-zinc-800 light:border-blue-100 border-b-2 text-neutral-300 light:text-slate-500 text-sm p-3.25'>
+            <Link to='#'>سریال</Link>
           </li>
-          <li>
-            <Link to=''>بهترین های IMDB</Link>
+          <li className='border-solid border-zinc-800 light:border-blue-100 border-b-2 text-neutral-300 light:text-slate-500 text-sm p-3.25'>
+            <Link to='#'>بهترین های IMDB</Link>
           </li>
-          <li>
-            <Link to=''>لیست های کاربران</Link>
+          <li className='border-solid border-zinc-800 light:border-blue-100 border-b-2 text-neutral-300 light:text-slate-500 text-sm p-3.25'>
+            <Link to='#'>لیست های کاربران</Link>
           </li>
-          <li>
-            <Link to=''>بازیگران</Link>
+          <li className='border-solid border-zinc-800 light:border-blue-100 border-b-2 text-neutral-300 light:text-slate-500 text-sm p-3.25'>
+            <Link to='#'>بازیگران</Link>
           </li>
-          <li>
-            <Link to=''>کالکشن</Link>
+          <li className='border-solid border-zinc-800 light:border-blue-100 border-b-2 text-neutral-300 light:text-slate-500 text-sm p-3.25'>
+            <Link to='#'>کالکشن</Link>
           </li>
-          <li>
-            <Link to=''>باکس آفیس</Link>
+          <li className='border-solid border-zinc-800 light:border-blue-100 border-b-2 text-neutral-300 light:text-slate-500 text-sm p-3.25'>
+            <Link to='#'>باکس آفیس</Link>
           </li>
         </ul>
       </div>
 
-      <div>
-        <button className='font-Estedad-SemiBold text-xs xl:text-sm bg-blue-950 py-3.5 px-4 rounded-15px'>ورود</button>
-        <button className='font-Estedad-SemiBold text-xs xl:text-sm bg-blue-950 py-3.5 px-4 rounded-15px'>عضویت</button>
-        <button onClick={toggleTheme} className='*:text-blue-500 py-3.5 px-4 rounded-15px'>
+      <div className='bg-zinc-800 light:bg-white absolute bottom-0 w-full mt-5 flex flex-col sm:flex-row items-center gap-2.5 p-2.5'>
+        <div className='w-full flex flex-col xs300px:flex-row gap-2.5 flex-1'>
+          <button className='font-Estedad-SemiBold xl:text-sm bg-blue-950 py-2 px-5 rounded-4xl text-sm w-full mx-0.5'>ورود</button>
+          <button className='font-Estedad-SemiBold xl:text-sm bg-blue-950 py-2 px-5 rounded-4xl text-sm w-full mx-0.5'>عضویت</button>
+        </div>
+        <button onClick={toggleTheme} className='*:text-blue-500 border-solid border-blue-700 border-2 rounded-full size-10 flexCenter'>
           {theme === 'dark' ? <CiSun size={22} /> : <IoMoonOutline size={18} />}
         </button>
       </div>
