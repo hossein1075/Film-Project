b8fce34095dce25162c9ca271f15ee80e1f6e656
hossein hosseini <hosseinh1075@gmail.com>
2026-02-11 21:46:21 +0330

    add html header template

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import NavDesktop from './NavDesktop'
 import NavMobile from './NavMobile'
+import { CiPlay1 } from "react-icons/ci";
+
 
 function Header() {
   return (
     <header>
-      <NavDesktop/>
-      <NavMobile/>
+      <div className='container'>
+        <NavDesktop />
+        <NavMobile />
+        <div>
+          <span>
+            TV-MA
+          </span>
+          <div>
+            <h2>
+              The Witcher
+            </h2>
+           <span>IMBD</span>
+           <div>
+            <span>4.5</span>
+            <span>/10</span>
+           </div>
+          </div>
+          <div>
+            <CiPlay1 />
+          </div>
+
+          {/* <img src="/Images/header/bg-1.webp" alt="image films" /> */}
+        </div>
+
+      </div>
     </header>
   )
 }
